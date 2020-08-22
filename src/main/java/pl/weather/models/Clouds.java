a288887b6262c5d4c40e6178ca8a542f3fcc4aa0
user <janacagnieszka@gmail.com>
2020-08-22 15:42:00 +0200

    stw. modeli api weather

--- src/main/java/pl/weather/models/Clouds.java
+++ src/main/java/pl/weather/models/Clouds.java
@@ -4,4 +4,15 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Clouds {
+    @SerializedName("all")
+    @Expose
+    private double all;
+
+    public double getAll() {
+        return all;
+    }
+
+    public void setAll(double all) {
+        this.all = all;
+    }
 }
--- src/main/java/pl/weather/models/Sys.java
+++ src/main/java/pl/weather/models/Sys.java
@@ -5,5 +5,75 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Sys {
+    @SerializedName("type")
+    @Expose
+    private double type;
 
+    @SerializedName("id")
+    @Expose
+    private long id;
+
+    @SerializedName("message")
+    @Expose
+    private double message;
+
+    @SerializedName("country")
+    @Expose
+    private String country;
+
+    @SerializedName("sunrise")
+    @Expose
+    private long sunrise;
+
+    @SerializedName("sunset")
+    @Expose
+    private long sunset;
+
+    public double getType() {
+        return type;
+    }
+
+    public void setType(double type) {
+        this.type = type;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getMessage() {
+        return message;
+    }
+
+    public void setMessage(double message) {
+        this.message = message;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public long getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(long sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public long getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(long sunset) {
+        this.sunset = sunset;
+    }
 }
