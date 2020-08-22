3a716b278fc681645f23e4a0db093e4b9cb7d10d
Adam <lilith213@o2.pl>
2020-08-22 15:45:37 +0200

    Entered infos from website

--- src/main/java/pl/weather/models/Sys.java
+++ src/main/java/pl/weather/models/Sys.java
@@ -6,4 +6,75 @@ import com.google.gson.annotations.SerializedName;
 
 public class Sys {
 
+    @SerializedName("type")
+    @Expose
+    private int type;
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+
+    @SerializedName("message")
+    @Expose
+    private double message;
+
+    @SerializedName("country")
+    @Expose
+    private String country;
+
+    @SerializedName("sunrise")
+    @Expose
+    private double sunrise;
+
+    @SerializedName("sunset")
+    @Expose
+    private double sunset;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getMessage() {
+        return message;
+    }
+
+    public void setMessage(double message) {
+        this.message = message;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public double getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(double sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public double getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(double sunset) {
+        this.sunset = sunset;
+    }
 }
--- src/main/java/pl/weather/models/Weather.java
+++ src/main/java/pl/weather/models/Weather.java
@@ -6,4 +6,48 @@ import com.google.gson.annotations.SerializedName;
 
 public class Weather {
 
+    @SerializedName("id")
+    @Expose
+    private double id;
+    @SerializedName("main")
+    @Expose
+    private String main;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("icon")
+    @Expose
+    private String icon;
+
+    public double getId() {
+        return id;
+    }
+
+    public void setId(double id) {
+        this.id = id;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public void setMain(String main) {
+        this.main = main;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
 }
