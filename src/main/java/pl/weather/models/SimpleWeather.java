2cd5b294de52b72197cadfe7eab28d95089d55a6
dennnis0204 <dennnis0204@gmail.com>
2020-08-23 16:29:27 +0200

    Changed getters setters to lombok @Getter @Setter

--- src/main/java/pl/weather/models/SimpleWeather.java
+++ src/main/java/pl/weather/models/SimpleWeather.java
@@ -1,5 +1,12 @@
 package pl.weather.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class SimpleWeather {
     private String name;
     private String description;
@@ -8,58 +15,10 @@ public class SimpleWeather {
     private int pressure;
     private int humidity;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setTemp(double temp) {
-        this.temp = temp;
-    }
-
-    public void setFeelsLike(double feelsLike) {
-        this.feelsLike = feelsLike;
-    }
-
-    public void setPressure(int pressure) {
-        this.pressure = pressure;
-    }
-
-    public void setHumidity(int humidity) {
-        this.humidity = humidity;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public double getTemp() {
-        return temp;
-    }
-
-    public double getFeelsLike() {
-        return feelsLike;
-    }
-
-    public int getPressure() {
-        return pressure;
-    }
-
-    public int getHumidity() {
-        return humidity;
-    }
-
     @Override
     public String toString() {
         return "Pogoda w: " + name +
-                ", opis:" + description +
+                ", opis: " + description +
                 ", temperatura: " + temp +
                 ", temp odczuwalna: " + feelsLike +
                 ", cisnienie: " + pressure +
