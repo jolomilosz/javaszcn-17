b61d2bcf9a6ef8be12955aa4e0bca932d014e15c
dennnis0204 <dennnis0204@gmail.com>
2020-08-23 21:03:20 +0200

    Created units for UrlProvider methods

--- src/main/java/pl/weather/Units.java
+++ src/main/java/pl/weather/Units.java
@@ -1,5 +1,16 @@
 package pl.weather;
 
 public enum Units {
+    METRIC("metric"),
+    IMPERIAL("imperial");
 
+    public final String units;
+
+    Units(String units) {
+        this.units = units;
+    }
+
+    public String getUnits() {
+        return units;
+    }
 }
