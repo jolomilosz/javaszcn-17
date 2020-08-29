3e27a5b001edb0bed8e6e61a397d278264ede156
dennnis0204 <dennnis0204@gmail.com>
2020-08-29 13:04:48 +0200

    Created custom WeatherApiException

--- src/main/java/pl/weather/api/WeatherApiException.java
+++ src/main/java/pl/weather/api/WeatherApiException.java
@@ -1,11 +1,25 @@
 package pl.weather.api;
 
-public class WeatherApiException extends RuntimeException {
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class WeatherApiException extends IOException {
 
     /**
      * TODO
      *  1. Podaj wiadomość do klasy bazowej
      *  2. Stwórz pole dla kodu błędu
      */
+
+    private String errorMessage;
+
+    public WeatherApiException(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String toString() {
+        return "WeatherApiException: " + errorMessage;
+    }
 
 }
