9d22ef85ab01e563a4ef6470887128e92ad9cbb3
dennnis0204 <dennnis0204@gmail.com>
2020-08-30 09:35:09 +0200

    Added write to file/database functionality

--- src/main/java/pl/weather/Main.java
+++ src/main/java/pl/weather/Main.java
@@ -1,6 +1,7 @@
 package pl.weather;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import pl.weather.api.WeatherListener;
 import pl.weather.models.SimpleWeather;
@@ -16,35 +17,57 @@ import java.util.Optional;
 import java.util.Scanner;
 
 public class Main implements WeatherListener {
-    private static final Settings DEFAULT_SETTIGS = new Settings("Szczecin", TimePeriod.NORMAL.getTimeInMillis());
+    private static final Settings DEFAULT_SETTINGS = new Settings("Szczecin", TimePeriod.NORMAL.getTimeInMillis());
 
     public static void main(String[] args) {
         DataFileManager<Settings> fileManager = new DataFileManager<>(new FileStorage());
         Optional<Settings> settingsFromFile = fileManager.readFromFile(Settings.class);
-        Settings settings = settingsFromFile.orElse(DEFAULT_SETTIGS);
+        Settings settings = settingsFromFile.orElse(DEFAULT_SETTINGS);
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
-        WeatherIntervalCheckTask zadanie = new WeatherIntervalCheckTask(new Main(), settings.getLastCheckLocation());
+        WeatherIntervalCheckTask task = new WeatherIntervalCheckTask(new Main(), settings.getLastCheckLocation());
 
         WeatherIntervalCheckExecutor checkExecutor = new WeatherIntervalCheckExecutor();
-        checkExecutor.checkInInterval(zadanie, TimePeriod.getPeriodEquals(settings.getLastUsedInterval()));
+        checkExecutor.checkInInterval(task, TimePeriod.getPeriodEquals(settings.getLastUsedInterval()));
 
         Scanner scanner = new Scanner(System.in);
         if (scanner.hasNext()) {
             checkExecutor.stopChecking();
+            sessionFactory.close();
         }
 
 
     }
 
-
     @Override
     public void onSuccess(SimpleWeather simpleWeather) {
-        System.out.println(simpleWeather);
-    }
 
+        System.out.println(simpleWeather);
+
+        // write to file
+        DataFileManager<SimpleWeather> fileManager = new DataFileManager<>(new FileStorage());
+        fileManager.writeToFile(simpleWeather);
+
+        // write to database
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save simpleWeather
+            session.save(simpleWeather);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void onFail(String errorMassage) {
         System.out.println("ups: " + errorMassage);
     }
+
 }
--- src/main/java/pl/weather/Settings.java
+++ src/main/java/pl/weather/Settings.java
@@ -1,27 +1,15 @@
 package pl.weather;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString()
 public class Settings {
     private String lastCheckLocation;
     private long lastUsedInterval;
-
-    public Settings(String lastCheckLocation, long lastUsedInterval) {
-        this.lastCheckLocation = lastCheckLocation;
-        this.lastUsedInterval = lastUsedInterval;
-    }
-
-    public String getLastCheckLocation() {
-        return lastCheckLocation;
-    }
-
-    public void setLastCheckLocation(String lastCheckLocation) {
-        this.lastCheckLocation = lastCheckLocation;
-    }
-
-    public long getLastUsedInterval() {
-        return lastUsedInterval;
-    }
-
-    public void setLastUsedInterval(long lastUsedInterval) {
-        this.lastUsedInterval = lastUsedInterval;
-    }
 }
