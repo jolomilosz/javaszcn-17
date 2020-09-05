f0ebb3160c1a66e205266c24bfa387f8ba01f74f
Vincer0 <pasierb.adam@gmail.com>
2020-09-05 10:58:03 +0200

    fix: Added missing imports

--- src/main/java/pl/weather/tools/intervalchecker/WeatherIntervalCheckTask.java
+++ src/main/java/pl/weather/tools/intervalchecker/WeatherIntervalCheckTask.java
@@ -2,8 +2,11 @@ package pl.weather.tools.intervalchecker;
 
 import pl.weather.api.CloudApi;
 import pl.weather.api.WeatherApi;
+import pl.weather.api.WeatherApiException;
 import pl.weather.api.WeatherListener;
+import pl.weather.models.SimpleWeather;
 
+import java.io.IOException;
 import java.util.TimerTask;
 
 public class WeatherIntervalCheckTask extends TimerTask {
